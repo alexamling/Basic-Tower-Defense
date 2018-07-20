c447f65ee01f98ef35929099378c0ed1fb591a0f
Alexander Amling <aja7073@rit.edu>
2018-07-20 19:34:40 -0400

    Basic Enemy Movement
    Enemy moves along the x-axis, and bobs up and down in a sine wave pattern

---
+++ Basic Tower Defense/Assets/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject enemy;
+
+    private List<Enemy> enemies;
+
+	// Use this for initialization
+	void Start () {
+        enemies = new List<Enemy>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
